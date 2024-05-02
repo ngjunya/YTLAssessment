ce7e7e9aaa34d642be634b50dd3c253f11051196
ngjunya <65483068+ngjunya@users.noreply.github.com>
2024-05-02 19:24:05 +0800

    chore: added rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    "@react-native",
+    "plugin:react-hooks/recommended",
+  ],
+  rules: {
+    "prettier/prettier": "off",
+    "no-console": "error",
+    "semi": ["error", "always"],
+    "no-unused-vars": "error",
+    "quotes": ["error", "double"],
+    "indent": ["error", 2],
+    "react/prop-types": "warn",
+    "react/self-closing-comp": "warn",
+    "react/no-unknown-property": "error",
+    "react-native/no-inline-styles": "warn",
+  }
 };
+
